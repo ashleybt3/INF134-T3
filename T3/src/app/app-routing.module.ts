60c7d5be0b7df73cab1c0963ff25ff108ae3bb0b
RawlinStone <rawlinstone@hotmail.com>
2021-05-24 18:47:21 -0700

    Update app-routing.module.ts

--- T3/src/app/app-routing.module.ts
+++ T3/src/app/app-routing.module.ts
@@ -5,15 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'home-page',
-    loadChildren: () => import('./home-page/home-page.module').then( m => m.HomePagePageModule)
   }
+
 ];
 @NgModule({
   imports: [
