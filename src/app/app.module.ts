083dd23efc4b7810f34c2cf5c7177164a41a363f
Ashley Teves <ashleybtev@gmail.com>
2021-06-01 12:57:02 -0700

    move files

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {MapModalComponent} from './map-modal/map-modal.component';
+
+
+@NgModule({
+  declarations: [AppComponent, MapModalComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/modals/my-modal/my-modal.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { 
+  ModalController, 
+  NavParams 
+  } from '@ionic/angular';
+
+@Component({
+  selector: 'app-my-modal',
+  templateUrl: './my-modal.page.html',
+  styleUrls: ['./my-modal.page.scss'],
+})
+
+export class MyModalPage implements OnInit {
+
+  constructor(
+    public modalController: ModalController,
+    public navParams: NavParams
+  ) { }
+
+  ngOnInit() {
+    console.table(this.navParams);
+  }
+
+  async closeModal() {
+    await this.modalController.dismiss();
+  }
+
+  public buttonColor: string = "gray";
+
+  changeColor() {
+    this.buttonColor = "#FFF";
+
+  }
+
+}
